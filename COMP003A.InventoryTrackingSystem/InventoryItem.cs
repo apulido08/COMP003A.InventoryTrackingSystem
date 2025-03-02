fe05f5722b4346d3d334359ef76cfbd951183232
Alvaro Pulido <alavropulido@my.whccd.edu>
2025-03-02 14:38:07 -0800

    added two new classes to file

--- COMP003A.InventoryTrackingSystem/InventoryItem.cs
+++ COMP003A.InventoryTrackingSystem/InventoryItem.cs
@@ -7,10 +7,8 @@ namespace COMP003A.InventoryTrackingSystem
 {
     public class InventoryItem
     {
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public int Quantity { get; set; }
-        public decimal Price { get; set; }
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- COMP003A.InventoryTrackingSystem/InventoryManager.cs
+++ COMP003A.InventoryTrackingSystem/InventoryManager.cs
@@ -1,5 +1,7 @@
 ﻿// Author: [Alvaro Pulido]
 
+using System.Security.Cryptography.X509Certificates;
+
 namespace COMP003A.InventoryTrackingSystem
 {
     public class InventoryManager
@@ -7,8 +9,41 @@ namespace COMP003A.InventoryTrackingSystem
         private List<InventoryItem> items = new List<InventoryItem>();
         private int nextId = 1;
 
-      public void AddItem(string name, int quantity)
+        public void AddItem(string name, int quantity)
         {
             items.Add(new InventoryItem { Id = nextId++, Name = name });
+            Console.WriteLine("Item added successfully!.");
+        }
+
+        public void ViewItems()
+        {
+            Console.WriteLine("Inventory");
+            foreach (InventoryItem item in items)
+            {
+                Console.WriteLine($"Id: {item.Id}, Name: {item.Name}, Quantity: {item.Quantity}");
+
+            }
+
+            
+
+        }
+        public void RemoveItem(int id)
+        {
+            InventoryItem item = items.Find(item => item.Id == id);
+            if (items != null)
+            {
+                items.Remove(item);
+                Console.WriteLine("Item removed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Item not found.");
+            }
+        }
+
+        public void UpdateItem(int id, string name, int quantity)
+        {
+
+        }
     }
 }
