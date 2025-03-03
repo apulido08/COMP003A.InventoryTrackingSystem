76a262b1d6e466dc13cf56d24500adb0652f186b
Alvaro Pulido <alavropulido@my.whccd.edu>
2025-03-02 18:16:03 -0800

    Notes added to Inventory item section

--- COMP003A.InventoryTrackingSystem/InventoryItem.cs
+++ COMP003A.InventoryTrackingSystem/InventoryItem.cs
@@ -5,10 +5,24 @@
 
 namespace COMP003A.InventoryTrackingSystem
 {
+    /// <summary>
+    /// represents inventory items based off name, id, and quantity
+    /// </summary>
     public class InventoryItem
     {
+        /// <summary>
+        /// Calls for the Id of the item
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Calls for the name of the item
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Calls for the quantity of the item
+        /// </summary>
         public int Quantity { get; set; }
     }
 
